6bec2178b2f3a8938d86232c01d1c30c3890ecbd
Jeremiah VALERIE <jeremiah@overblog.com>
2020-05-10 11:11:45 +0200

    Upgrade to symfony/mercure >=0.2

--- tests/SubscriptionManagerTest.php
+++ tests/SubscriptionManagerTest.php
@@ -11,9 +11,15 @@ use Overblog\GraphQLSubscription\Storage\MemorySubscriptionStorage;
 use Overblog\GraphQLSubscription\SubscriptionManager;
 use Overblog\GraphQLSubscription\Update;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
 use Symfony\Component\Mercure\Publisher;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\MessageBus;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Contracts\HttpClient\ResponseStreamInterface;
 
 class SubscriptionManagerTest extends TestCase
 {
@@ -43,7 +49,8 @@ class SubscriptionManagerTest extends TestCase
     public function testEventStopPropagation(): void
     {
         $executor = $this->createCallableMock();
-        $httpClient = $this->createCallableMock();
+        $responseFactory = $this->createCallableMock();
+        $httpClient = new MockHttpClient($responseFactory);
         $subscriptionManager = $this->createSubscriptionManager(
             $this->createPublisher([], $httpClient),
             $executor
@@ -57,7 +64,7 @@ class SubscriptionManagerTest extends TestCase
                 return new ExecutionResult(['inbox' => $rootValue->getPayload()]);
             })
         ;
-        $httpClient
+        $responseFactory
             ->expects($this->never())
             ->method('__invoke');
         $subscriptionManager->notify('inbox', []);
@@ -139,19 +146,19 @@ class SubscriptionManagerTest extends TestCase
         return $this->createPartialMock(\stdClass::class, ['__invoke']);
     }
 
-    private function createPublisher(array $expectedPostData = [], callable $httpClient = null, ?callable $jwtProvider = null, ?string $hubUrl = self::HUB_URL): Publisher
+    private function createPublisher(array $expectedPostData = [], HttpClientInterface $httpClient = null, ?callable $jwtProvider = null, ?string $hubUrl = self::HUB_URL): Publisher
     {
         return new Publisher(
             $hubUrl,
             $jwtProvider ?? function (): string {
                 return self::JWT;
             },
-            $httpClient ?? function (string $url, string $jwt, string $postData) use ($expectedPostData): string {
-                \parse_str($postData, $actualPostData);
+            $httpClient ?? new MockHttpClient(function (string $method, string $url, array $options) use ($expectedPostData): ResponseInterface {
+                \parse_str($options['body'], $actualPostData);
                 $this->assertSame($expectedPostData, $actualPostData);
 
-                return 'myID';
-            }
+                return new MockResponse('myID');
+            })
         );
     }
 }
